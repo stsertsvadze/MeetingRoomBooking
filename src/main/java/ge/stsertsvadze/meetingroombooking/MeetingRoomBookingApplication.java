4b2de0e100019c5b27fc95686dfe316c49e7f46f
stsertsvadze <stser18@freeuni.edu.ge>
2021-07-12 23:52:58 +0400

    initial commit

---
+++ src/main/java/ge/stsertsvadze/meetingroombooking/MeetingRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package ge.stsertsvadze.meetingroombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingApplication.class, args);
+	}
+
+}
